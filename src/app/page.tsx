9a9ec36e22b3b53185c07c0a6aba17215e375ae4
blhanagami@gmail.com <blhanagami@gmail.com>
2024-09-24 15:43:51 -1000

    made lots of changes to the page.tsx. Changed schema so it now uses the postgres dataset. Carried over urls to the dataset we renamed into images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { db } from "~/server/db";
 
-const mockUrls = [
+/*const mockUrls = [
   "https://utfs.io/f/d07ca49d-9377-4d0c-81c3-f161ec21a45a-bh75y4.jpg",
   "https://utfs.io/f/3402cab4-5e26-4e37-910b-d60f654f3dc5-bh75y3.jpg",
 ];
@@ -10,17 +10,20 @@ const mockImages = mockUrls.map((url, index) => ({
   id: index + 1,
   url,
 }));
-
+*/
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
+
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
+
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {posts.map((post) => (
-          <div key={post.id}>{post.name}</div>))}
-        {[...mockImages, ...mockImages, ...mockImages, ...mockImages, ...mockImages, ...mockImages].map((image) => (
-          <div key={image.id} className="w-48">
+        {[...images, ...images, ...images, ...images, ...images, ...images].map((image, index) => (
+          <div key={image.id + "-" + index} className="w-48 flex-col">
             <img src={image.url} />
+            <div>{image.name}</div>
       </div>
       ))}
       </div>
